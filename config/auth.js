d688b0ce2cfee734152e52113b5ebd94276812dc
Daniel Wilson <wilsoda@umich.edu>
2015-01-28 09:57:22 -0500

    fb callbackurl

--- config/auth.js
+++ config/auth.js
@@ -7,7 +7,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : 576135949186286, // your App ID
         'clientSecret'  : '8a64a0fa23ef2c44b6c7803fc5ebee28', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : '/auth/facebook/callback'
     },
 
     'twitterAuth' : {
