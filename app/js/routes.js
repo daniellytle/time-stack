01893e67030effdfa33d3792bfa66fa9194bf019
Daniel Wilson <wilsoda@umich.edu>
2015-01-29 16:18:17 -0500

    init

--- app/js/routes.js
+++ app/js/routes.js
@@ -3,22 +3,20 @@ module.exports = function(app, passport) {
 
 	var User = require('../models/user');
 
+	// APP ROUTING ===================================
+
 	//initial landing
 	app.get('/', isLoggedIn, function(req, res) {
-		console.log(req.user);
 		res.render('app', {user : req.user});
 	});
 
-
 	app.get('/login', function(req, res) {
-		console.log('got login');
-		res.render('index', {user : req.user});
+		res.render('login', {user : req.user});
 	});
 
 	app.get('/logout', function(req, res){
-		console.log('logoout');
-	  req.logout();
-	  res.redirect('/login');
+		req.logout();
+		res.redirect('/login');
 	});
 
 	// API ROUTES =====================================
@@ -28,28 +26,30 @@ module.exports = function(app, passport) {
 		console.log('post');
 		User.findOneAndUpdate({facebookId: req.body.Fbid},
 			{$push: {todos: req.body.todo}},
-	    {safe: true, upsert: true},
-	    function(err,model) {
-	      console.log(err);
+			{safe: true, upsert: true},
+			function(err,model) {
+				console.log(err);
 				console.log(model);
 				res.send(model.todos[model.todos.length-1].id);
-	    })
+			}
+		)
 	});
 
 	app.delete('/api', function(req, res) {
 		console.log("delete");
 		console.log(req.query);
 		User.update(
-		  { facebookId: req.query.FbId },
-		  { $pull: { todos : { _id: req.query.Id } } }
-		, function(err, success) {
-			if (err)
+			{ facebookId: req.query.FbId },
+			{ $pull: { todos : { _id: req.query.Id } } }
+			, function(err, success) {
+				if (err)
 				console.log(err);
-			else {
-				console.log("success");
-				res.sendStatus(200);
+				else {
+					console.log("success");
+					res.sendStatus(200);
+				}
 			}
-		});
+		);
 	});
 
 	// FACEBOOK ROUTES ====================================
@@ -57,25 +57,26 @@ module.exports = function(app, passport) {
 	app.get('/auth/facebook', passport.authenticate('facebook', {
 		scope : ['public_profile', 'email'],
 		profileFields : ['id', 'displayName', 'emails','photos', 'picture']
-		}));
+	}));
 
-		// handle the callback after facebook has authenticated the user
+	// handle the callback after facebook has authenticated the user
 	app.get('/auth/facebook/callback',
 		passport.authenticate('facebook', {
 			successRedirect : '/',
 			failureRedirect : '/login'
 		})
 	);
+
 }
 
+// UTILITY ============================================
+
 function isLoggedIn(req, res, next) {
-
-		// if user is authenticated in the session, carry on
-		if (req.isAuthenticated()) {
-			console.log('good auth');
-			return next();
-		}
-
-		// if they aren't redirect them to the home page
-		res.redirect('/login');
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated()) {
+		console.log('good auth');
+		return next();
+	}
+	// if they aren't redirect them to the home page
+	res.redirect('/login');
 }
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 
+/* TimeStack - Node Server - Daniel Wilson 2015 */
+
 var express     = require('express');
 var app         = express();
 var passport    = require('passport');
@@ -7,36 +9,33 @@ var mongoose    = require('mongoose');
 var port        = process.env.PORT || 8080;
 var configDB    = require('./config/database.js');
 var session     = require('express-session');
-var cookieP     = require('cookie-parser');
+var cookieParse = require('cookie-parser');
 
 // DB setup
 mongoose.connect(configDB.url);
+// Session Parsing
+app.use(cookieParse('dothething'));
+// Express Session
+app.use(session({ secret : 'dothething' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Initial setup
+app.use(bodyParser());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
-  app.use(cookieP('dothething'));
-  // Express Session
-  app.use(session({ secret : 'dothething' })); // session secret
-  app.use(passport.initialize());
-  app.use(passport.session());
+// Config Passport
+require('./config/passport')(passport);
+// Load API Routes
+require('./app/js/routes.js')(app, passport);
 
-  // Initial setup
-  app.use(bodyParser());       // to support JSON-encoded bodies
-  app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-  }));
-
-  require('./config/passport')(passport);
-  require('./app/js/routes.js')(app, passport);
-
-
-  // Expose static assets
-  app.use(express.static(__dirname + '/public'));
-  app.set('views', __dirname + '/app/client');
-  app.set('view engine', 'jade');
-
-
-
-
+// Expose static assets
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/app/client');
+// Config Jade
+app.set('view engine', 'jade');
 
 app.listen(port);
 console.log('listening on port ' + port);
