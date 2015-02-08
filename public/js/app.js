9e47ae8e430ab4af331503493928682452303903
Daniel Wilson <wilsoda@umich.edu>
2015-02-08 00:48:16 -0500

    fixed sorting and adding

--- public/js/app.js
+++ public/js/app.js
@@ -4,159 +4,185 @@
 console.log('app.js');
 
 if ( (location.hash == "#_=_" || location.href.slice(-1) == "#_=_") ) {
-    removeHash();
+  removeHash();
 }
 
 var date;
 
 function removeHash() {
-    var scrollV, scrollH, loc = window.location;
-    if ('replaceState' in history) {
-        history.replaceState('', document.title, loc.pathname + loc.search);
-    } else {
-        // Prevent scrolling by storing the page's current scroll offset
-        scrollV = document.body.scrollTop;
-        scrollH = document.body.scrollLeft;
+  var scrollV, scrollH, loc = window.location;
+  if ('replaceState' in history) {
+    history.replaceState('', document.title, loc.pathname + loc.search);
+  } else {
+    // Prevent scrolling by storing the page's current scroll offset
+    scrollV = document.body.scrollTop;
+    scrollH = document.body.scrollLeft;
 
-        loc.hash = '';
+    loc.hash = '';
 
-        // Restore the scroll offset, should be flicker free
-        document.body.scrollTop = scrollV;
-        document.body.scrollLeft = scrollH;
-    }
+    // Restore the scroll offset, should be flicker free
+    document.body.scrollTop = scrollV;
+    document.body.scrollLeft = scrollH;
+  }
 }
 
 
 var picker = new Pikaday(
-    {
-        field: document.getElementById('dateHolder'),
-        minDate: new Date(),
-        // format: 'MMM DD',
-        yearRange:[new Date().getFullYear(),2020],
-        onSelect: function() {
-            date = this.getMoment();
-            $('#datepicker').css({background:'none'});
-            $('#datepicker').text(this.getMoment().format('MMM DD'));
-        }
-    });
+  {
+    field: document.getElementById('dateHolder'),
+    minDate: new Date(),
+    // format: 'MMM DD',
+    yearRange:[new Date().getFullYear(),2020],
+    onSelect: function() {
+      date = this.getMoment();
+      $('#datepicker').css({background:'none'});
+      $('#datepicker').text(this.getMoment().format('MMM DD'));
+    }
+  });
 
-//Inital Load
-data.todos.sort(function(a,b) {return a.dueDate < b.dueDate});
+  //Inital Load
+  data.todos.sort(function(a,b) {
+    console.log(a.dueDate);
+    console.log(b.dueDate);
+    return moment(a.dueDate) < moment(b.dueDate);
+  });
 
-for (var i = data.todos.length - 1; i >= 0; i--) {
-	 var create = moment(data.todos[i].createDate);
-	 var due 	= moment(data.todos[i].dueDate);
-	 var today 	= moment();
+  for (var i = data.todos.length - 1; i >= 0; i--) {
+    var create = moment(data.todos[i].createDate);
+    var due 	= moment(data.todos[i].dueDate);
+    var today 	= moment();
 
-	 var diff = due.diff(create, 'days');
-	 var task = $("<div class='tOut'><div class='todo'>"+ data.todos[i].name +
+    var diff = due.diff(create, 'days');
+    var task = $("<div class='tOut'><div class='todo'>"+ data.todos[i].name +
     "</div><div class='status'>"+ (diff + 1) +"</div><div class='icon'></div></div>");
 
-
-	 if(diff < 2) {
-	 	task.addClass('urgent').children('.icon').addClass('urgPic');
-	 } else if(diff < 4) {
-	 	task.addClass('mild').children('.icon').addClass('mildPic');
-	 } else
-	 	task.addClass('fine').children('.icon').addClass('finePic');
-
-	task.appendTo('.todos').fadeIn('slow');
-};
-
-//Enter Click
-$('body').keydown(function (e){
-    if(e.keyCode == 13){
-        $('#goButton').click();
-    }
-})
-
-//Submit new todo
-$('#goButton').click(function() {
-	//check empty
-	if($('#task').val() == "") {
-		//$('.newTop').addClass('empty');
-		return;
-	}
-
-	if(date == undefined) {
-		//$('#dateHolder').addClass('empty');
-		return;
-	}
-
-	 var create = moment();
-	 var due 	= date;
-
-	console.log(create);
-  console.log(due);
-
-	var diff = due.diff(create, 'days');
-  console.log(diff);
- 	var task = $("<div class='tOut'><div class='maybe todo'>"+ $('#task').val() +
-   "</div><div class='status'>"+ (diff+1) +"</div><div class='icon'></div></div>");
-
-
-   if(diff < 2) {
-     task.addClass('urgent').children('.icon').addClass('urgPic');
-   } else if(diff < 4) {
-     task.addClass('mild').children('.icon').addClass('mildPic');
-   } else
-     task.addClass('fine').children('.icon').addClass('finePic');
+    console.log(diff);
+    if(diff < 2) {
+      task.addClass('urgent').children('.icon').addClass('urgPic');
+    } else if(diff < 4) {
+      task.addClass('mild').children('.icon').addClass('mildPic');
+    } else
+    task.addClass('fine').children('.icon').addClass('finePic');
 
     task.appendTo('.todos').fadeIn('slow');
+  };
+
+  //Enter Click
+  $('body').keydown(function (e){
+    if(e.keyCode == 13){
+      $('#goButton').click();
+    }
+  })
+
+  //Submit new todo
+  $('#goButton').click(function() {
+    //check empty
+    if($('#task').val() == "") {
+      alert('please enter a task');
+      return;
+    }
+
+    if(date == undefined) {
+      alert('please enter a date');
+      return;
+    }
+
+    var create = moment();
+    var due 	= date;
+
+    console.log(create);
+    console.log(due);
+
+    var diff = due.diff(create, 'days');
+    console.log(diff);
+    var task = $("<div class='tOut'><div class='maybe todo'>"+ $('#task').val() +
+    "</div><div class='status'>"+ (diff+1) +"</div><div class='icon'></div></div>");
 
 
-  var newTask = {
-		name: $('#task').val(),
-		dueDate: due._d,
-    createDate: moment()._d
-	};
+    if(diff < 2) {
+      task.addClass('urgent').children('.icon').addClass('urgPic');
+    } else if(diff < 4) {
+      task.addClass('mild').children('.icon').addClass('mildPic');
+    } else
+    task.addClass('fine').children('.icon').addClass('finePic');
 
-	$.post("/api",{
-		Fbid 	: data.facebookId,
-		todo : newTask
-	},function(success, err) {
-  		console.log(success);
+    // find right spot
+    var i;
+    for(i = data.todos.length-1 ; i > -1; --i) {
+      console.log(moment(data.todos[i].dueDate));
+      console.log(due);
+
+      if(moment(data.todos[i].dueDate) > due) {
+          alert(i);
+          insertAtIndex(data.todos.length - i,task);
+          break;
+      }
+    }
+
+    if(i == -1)
+      insertAtIndex(data.todos.length - 1,task);
+
+    var newTask = {
+      name: $('#task').val(),
+      dueDate: due._d,
+      createDate: moment()._d
+    };
+
+    $.post("/api",{
+      Fbid 	: data.facebookId,
+      todo : newTask
+    },function(success, err) {
+      console.log(success);
       newTask._id = success;
       data.todos.push(newTask);
       console.log('removing');
-  		task.children('.maybe').removeClass('maybe');
+      task.children('.maybe').removeClass('maybe');
 
-	});
-})
+    });
+  })
 
-$('.newTop').click(function() {
-	$('#task').focus();
-});
+  $('.newTop').click(function() {
+    $('#task').focus();
+  });
 
-// Delete Todo
-$(document).on('click','.tOut', function() {
-  console.log('got delete');
-	//find which task
-	var i = 0;
-	for (i = data.todos.length - 1; i >= 0; i--) {
-		if(data.todos[i].name == $(this).children('.todo').text())
-			break;
-	};
+  // Delete Todo
+  $(document).on('click','.tOut', function() {
+    console.log('got delete');
+    //find which task
+    var i = 0;
+    for (i = data.todos.length - 1; i >= 0; i--) {
+      if(data.todos[i].name == $(this).children('.todo').text())
+      break;
+    };
 
-	var Id = data.todos[i]._id;
-	var FbId = data.facebookId;
-  data.todos.splice(i,1);
+    var Id = data.todos[i]._id;
+    var FbId = data.facebookId;
+    data.todos.splice(i,1);
 
-	var obj = $(this);
+    var obj = $(this);
 
-	//delete the task
-	$.ajax({
-	    url: '/api' + '?' + $.param({"Id": Id, "FbId" : FbId}),
-	    type: 'DELETE',
-	    success: function() {
-	    	console.log("good");
-	    	obj.fadeOut(500).remove();
-	    },
-	    error: function(err) {
-	    	obj.addClass('maybe');
-	    }
-	});
+    //delete the task
+    $.ajax({
+      url: '/api' + '?' + $.param({"Id": Id, "FbId" : FbId}),
+      type: 'DELETE',
+      success: function() {
+        console.log("good");
+        obj.fadeOut(500).remove();
+      },
+      error: function(err) {
+        obj.addClass('maybe');
+      }
+    });
 
-});
+  });
+
+function insertAtIndex(i, task) {
+    if(i === 0) {
+     $(".todos").prepend(task);
+     return;
+    }
+
+    $(".todos > div:nth-child(" + (i - 1) + ")").after(task);
+}
 
 },{}]},{},[1]);
